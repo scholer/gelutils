5708cc0148acee859cd7b396e3cc27472a2dae9c
Rasmus Scholer <rasmusscholer@gmail.com>
2016-09-23 18:42:29 -0400

    Updated batch files adding notice about them being obsolute and to use end-points created by pip instead.

--- bin/Annotate_gel.bat
+++ bin/Annotate_gel.bat
@@ -1,13 +1,11 @@
 @echo off
 REM %0 is filepath of this batch file. %~dp0 is drive and path of batch file.
-REM Alternatively, make a .lnk, but that requires an absolute path.
-REM Activate alternative environment (use 'call' when you are in a batch script).
-REM - Alternatively, call the environment-specific python.
-REM call activate oldpil
-REM "C:\Program Files (x86)\Anaconda\envs\oldpil\python.exe" %~dp0\..\gelutils\gelannotator_gui.py %1
+REM Activate python environment if needed, use 'call' when you are in a batch script.
+REM Alternatively, call the environment-specific python interpreter using its full path.
 
+REM Using annotate_gel.py script:
+echo OBS: Using batch scripts is obsolete! Use pip/conda to install and then use the created AnnotateGel executables in your Python's bin/ directory.
 call activate py3pip
-python %~dp0\..\gelutils\gelannotator_gui.py %1 --filename_substitution "-[SYBR Gold]" "" --openwebbrowser
-
+python.exe %~dp0\annotate_gel.py %*
 
 IF ERRORLEVEL 1 pause
